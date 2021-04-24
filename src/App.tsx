54c2c57a3723372c590a80fa934ff8e81d931f60
AnnaIrkhina <anka.irhina@gmail.com>
2021-04-24 15:06:30 -0700

    qqq

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+
 import './App.css';
+import Search from "./Search";
+import SearchResults from "./SearchResults";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const URL = 'https://api.themoviedb.org/3/search/movie';
+    const [query, setQuery] = useState('');
+
+     const [resultJSON, setResulJSON] = useState({data:{results:[]}})
+    useEffect(() => {
+        axios.get(URL, {
+            params: {
+                api_key: "d3d2e3261fa9b4aff72b9f65f45d26ce",
+                language: "en-US",
+                page:1,
+                include_adult: "false",
+                query: query,
+
+            },
+        })
+            .then(responce => setResulJSON(responce))
+            .catch(error => console.log(error))
+
+
+    }, [query])
+    return (
+
+        <div className="App-header">
+
+            <Search query = {query} setQuery = {setQuery}/>
+            <SearchResults results={resultJSON.data.results}/>
+        </div>
+    );
 }
 
 export default App;
